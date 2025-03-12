95fad1d7abe3a4761164ef57466d87fa1fa64265
shahid123s <shahidnoushad13s@gmail.com>
2025-03-12 10:39:01 +0530

    Style changed in the image

--- src/Pages/UpdatePage.jsx
+++ src/Pages/UpdatePage.jsx
@@ -4,7 +4,7 @@ import SiteLogo from '../assets/gravityLogo.png'
 function UpdatePage() {
   return (
    <div className = ' bg-[#121212] flex flex-col justify-center items-center h-screen w-[100%] font-poppins text-lg text-white '>
-    <img src={SiteLogo} alt="Gravital Logo" className='w-64 h-64 aspect-square' />
+    <img src={SiteLogo} alt="Gravital Logo" className='w-64 ' />
       This Feature will avaialble on next update.
       <p className='text-sm'>Please co-operative with us</p>
     </div>
