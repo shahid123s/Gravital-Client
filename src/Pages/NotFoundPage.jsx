938c95d75dbb0f5fa7c283d923bca92d5ab97cca
shahid123s <shahidnoushad13s@gmail.com>
2025-03-13 20:20:21 +0530

    Style Change in image

--- src/Pages/NotFoundPage.jsx
+++ src/Pages/NotFoundPage.jsx
@@ -6,7 +6,7 @@ function NotFoundPage() {
   return (
     <div className='bg-[#121212] flex flex-col justify-center items-center h-screen w-[100%] font-poppins text-lg text-white'>
     {/* Add the spin animation */}
-    <img src={SiteLogo} alt="Gravital Logo" className='w-64 animate-spin-slow' />
+    <img src={SiteLogo} alt="Gravital Logo" className='w-64 animate-spin-slow rounded-full' />
     This feature will be available in the next update.
     <p className='text-sm'>Please cooperate with us</p>
   </div>
