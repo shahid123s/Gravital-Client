484b621b20efe4a1b8271263d098b8664475681f
shahid123s <shahidnoushad13s@gmail.com>
2025-03-14 17:36:30 +0530

    Checking the change password functionality

--- src/Components/User/settingsComponent/PasswordChangeForm.jsx
+++ src/Components/User/settingsComponent/PasswordChangeForm.jsx
@@ -35,7 +35,7 @@ function PasswordChangeForm({setClose}) {
             setClose()
 
         } catch (error) {
-            toast.error(error.response.data.message)
+            toast.error(error.message || 'Error changing password')
         }
     }
     return (
